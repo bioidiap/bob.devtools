87d312b6c50edf5da830f0fe1d76e9ad08b03b76
Andre Anjos <andre.dos.anjos@gmail.com>
2019-01-09 15:34:21 +0100

    [ci] Simplify after build

--- _ci/after_build.sh
+++ _ci/after_build.sh
@@ -4,10 +4,7 @@ source $(dirname ${0})/functions.sh
 
 # delete the bob packages from the cache otherwise the cache keeps increasing
 # over and over. See https://gitlab.idiap.ch/bob/bob.admin/issues/65
-run_cmd rm -rf ${CONDA_ROOT}/pkgs/batl*.tar.bz2
-run_cmd rm -rf ${CONDA_ROOT}/pkgs/beat*.tar.bz2
 run_cmd rm -rf ${CONDA_ROOT}/pkgs/bob*.tar.bz2
-run_cmd rm -rf ${CONDA_ROOT}/pkgs/gridtk*.tar.bz2
 
 # run git clean to clean everything that is not needed. This helps to keep the
 # disk usage on CI machines to minimum.
