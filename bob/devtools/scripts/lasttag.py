f6cf88c9a08eda40fbff9918990e4d14d946fe9a
Andre Anjos <andre.dos.anjos@gmail.com>
2019-02-14 09:23:30 +0100

    [scripts] Add preliminary color support

--- bob/devtools/scripts/lasttag.py
+++ bob/devtools/scripts/lasttag.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
 import os
-import logging
-logger = logging.getLogger(__name__)
 
 import click
+import gitlab
 
 from . import bdt
-from ..log import verbosity_option
 from ..changelog import get_last_tag, parse_date
 from ..release import get_gitlab_instance
+
+from ..log import verbosity_option, get_logger, echo_normal, echo_warning
+logger = get_logger(__name__)
 
 
 @click.command(epilog='''
@@ -38,11 +39,16 @@ def lasttag(package):
     gl = get_gitlab_instance()
 
     # we lookup the gitlab package once
-    use_package = gl.projects.get(package)
-    logger.info('Found gitlab project %s (id=%d)',
-        use_package.attributes['path_with_namespace'], use_package.id)
+    try:
+      use_package = gl.projects.get(package)
+      logger.info('Found gitlab project %s (id=%d)',
+          use_package.attributes['path_with_namespace'], use_package.id)
 
-    tag = get_last_tag(use_package)
-    date = parse_date(tag.commit['committed_date'])
-    click.echo('Lastest tag for %s is %s (%s)' % \
-        (package, tag.name, date.strftime('%Y-%m-%d %H:%M:%S')))
+      tag = get_last_tag(use_package)
+      date = parse_date(tag.commit['committed_date'])
+      echo_normal('%s: %s (%s)' % \
+          (package, tag.name, date.strftime('%Y-%m-%d %H:%M:%S')))
+    except gitlab.GitlabGetError as e:
+      logger.warn('Gitlab access error - package %s does not exist?',
+          package)
+      echo_warning('%s: unknown' % (package,))
